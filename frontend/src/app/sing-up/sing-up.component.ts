9e99a24140e2a7ed88898460c452abf8d30afa62
savg92 <1992savg@gmail.com>
2023-12-21 21:44:20 -0300

    Update router links in header and sign-up components

--- frontend/src/app/sing-up/sing-up.component.ts
+++ frontend/src/app/sing-up/sing-up.component.ts
@@ -1,5 +1,4 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-sing-up',
@@ -7,8 +6,5 @@ import { Router } from '@angular/router';
 	styleUrls: ['./sing-up.component.scss'],
 })
 export class SingUpComponent {
-	router = inject(Router);
-	navigate() {
-		this.router.navigateByUrl('/login');
-	}
+	
 }
